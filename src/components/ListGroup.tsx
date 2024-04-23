6c0f81d23bc2c3f5c2bce4d1c6f1f2311f870949
davimafraa <davimafraaa@gmail.com>
2024-04-22 21:40:20 -0300

    Utilizando {items, heading} ao invés de props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,15 +5,15 @@ interface Props {
   heading: string;
 }
 
-function ListGroup(props: Props) {
+function ListGroup({items, heading}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>{props.heading}</h1>
-      {props.items.length === 0 && <p>No items found</p>}
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {props.items.map((item, index) => (
+        {items.map((item, index) => (
           <li
             className={
               selectedIndex === index
