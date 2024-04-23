688b0296057d0d7ddeb5f4084a347a20077d87de
davimafraa <davimafraaa@gmail.com>
2024-04-22 21:37:05 -0300

    Utilizando props ao invés de {items, heading}

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup(props: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
-      {items.length === 0 && <p>No items found</p>}
+      <h1>{props.heading}</h1>
+      {props.items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {props.items.map((item, index) => (
           <li
             className={
               selectedIndex === index
